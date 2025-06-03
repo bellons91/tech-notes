52f049a59829dbd73f603fb106b4d088351d4de0
Bellone Davide <Davide.bellone@xtech.ai>
2025-06-03 15:12:55 +0200

    updated theme

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Gli appunti di Davide 🐧",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
